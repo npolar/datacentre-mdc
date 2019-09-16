2ce8b32c47af71fa25fe79595588f71a9808254a
cnrdh <conrad@npolar.no>
2019-09-16 14:56:25 +0200

    Fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 
 const input = {
-  "button-mdc": "src/button/button-mdc.js",
-  "header-bar": "src/header/header-bar.js"
+  "button-mdc": "src/button/button-mdc.js"
+  //"header-bar": "src/header/header-bar.js"
 };
 
 const output = { format: "esm", dir: "dist" };
