6f1dc4f7dbab0ac386124974bedb6cbc3ca068a9
cnrdh <conrad@npolar.no>
2019-09-16 14:38:22 +0200

    Added header-bar to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 
 const input = {
-  "button-mdc": "src/button/button-mdc.js"
+  "button-mdc": "src/button/button-mdc.js",
+  "header-bar": "src/header/header-bar.js"
 };
 
 const output = { format: "esm", dir: "dist" };
