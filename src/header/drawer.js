1f4ed0c83460abeb7172c1830308aec25ffdae93
cnrdh <conrad@npolar.no>
2019-10-02 15:11:27 +0200

    More menu items in default drawer

--- src/header/drawer.js
+++ src/header/drawer.js
@@ -1,29 +1,49 @@
 import { html } from "lit-element";
+import "";
+
+export const drawerHeading = `Norwegian Polar
+Data Centre`;
+
+export const drawerSubheading = `https://data.npolar.no`;
+
+const buttonIcon = icon =>
+  html`
+    <button-icon icon=${icon}></button-icon>
+  `;
+
+export const ali = (text, { href, icon, tabindex }) => html`
+  <a class="mdc-list-item" href=${href} tabindex=${tabindex === 0 ? 0 : ""}>
+    ${icon ? buttonIcon(icon) : ""}
+    <li>${text}</li>
+  </a>
+`;
 export const footer = html`
-  <hr class="mdc-list-divider" />
-  <a class="mdc-list-item" href="/settings">
-    <button-icon icon="settings"></button-icon>
-    <li>Preferences</li>
-  </a>
-  <a class="mdc-list-item" href="/sign-in">
-    <button-icon icon="account_box"></button-icon>
-    <li>Sign in</li>
-  </a>
+  ${ali("Preferences", { href: "/preferences", icon: "settings" })}
+  ${ali("Sign in", { href: "/sign-in", icon: "account_box" })}
 `;
 
 export const drawer = html`
   <nav class="mdc-typography">
     <ul class="mdc-list">
-      <a class="mdc-list-item" href="#dataset" tabindex="0">
-        <span>Dataset catalogue</span>
-      </a>
-      <a class="mdc-list-item" href="#what-else?">
-        <span>What else?</span>
-      </a>
+      ${ali("Dataset catalogue", {
+        href: "/dataset",
+        icon: false,
+        tabindex: 0
+      })}
+      ${ali("Data APIs", {
+        href: "/api"
+      })}
+      ${ali("Data policy", {
+        href: "/about/data-policy"
+      })}
+      ${ali("Data publishing guides", {
+        href: "/guide"
+      })}
       <hr class="mdc-list-divider" />
-      <a class="mdc-list-item" href="#apps">
-        <span>More…</span>
-      </a>
+      ${ali("Applications", {
+        href: "/applications",
+        icon: "apps"
+      })}
       ${footer}
     </ul>
   </nav>
