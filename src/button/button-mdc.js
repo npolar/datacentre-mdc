84b00a03e6107f758b48f80af3e8c4783d8883ce
cnrdh <conrad@npolar.no>
2019-10-02 13:08:38 +0200

    Impoort MWC styles

--- src/button/button-mdc.js
+++ src/button/button-mdc.js
@@ -1,7 +1,9 @@
 import { style } from "./button-style.js";
-import { style as iconStyle} from "./button-icon-style.js";
 import { ButtonBase } from "@material/mwc-button/mwc-button-base.js";
 
-export class Button extends ButtonBase {}
-Button.styles = [iconStyle,style];
+export class Button extends ButtonBase {
+  static get styles() {
+    return [style];
+  }
+}
 customElements.define("button-mdc", Button);
