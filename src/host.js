482df035b895761a4b5f5aa2e2436788f8a042fc
cnrdh <conrad@npolar.no>
2019-11-06 18:11:13 +0100

    Clean

--- src/host.js
+++ src/host.js
@@ -2,15 +2,8 @@ export const emit = (host, name, detail) => {
   const event = new CustomEvent(name, {
     detail,
     bubbles: true,
-    composed: true
+    composed: true,
+    cancelable: true
   });
   host.dispatchEvent(event);
 };
-
-// toggle(e) {
-// data-toggle="search"
-//   const { toggle } = e.target.dataset;
-//   if ([true, false].includes(this[toggle])) {
-//     this[toggle] = !this[toggle];
-//   }
-// }
